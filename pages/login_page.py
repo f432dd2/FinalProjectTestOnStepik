6443781dc8ddcc0d7c86e404a8d01371f93a8388
f432dd2 <danilbask3462@gmail.com>
2022-05-16 20:23:30 +0300

    added commit 5 (LoginPage implementation)

---
+++ pages/login_page.py
@@ -0,0 +1,30 @@
+from lib2to3.pgen2 import driver
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        #assert self.is_element_present(*LoginPageLocators.LOGIN_URL), "URL not is correct"
+
+        assert LoginPageLocators.LOGIN_URL, "URL not is correct"
+
+        login_url = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
+        login_url.click()
+        #assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login field is not"
+        #assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register field is not"
+        #assert True
