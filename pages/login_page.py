504eef9ccbd40bc7d19778f232dfbbcb93f44b41
f432dd2 <danilbask3462@gmail.com>
2022-06-07 12:14:05 +0300

    added commit 6 (Page transitions)

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,21 +10,16 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        #assert self.is_element_present(*LoginPageLocators.LOGIN_URL), "URL not is correct"
 
         assert LoginPageLocators.LOGIN_URL, "URL not is correct"
 
         login_url = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
         login_url.click()
-        #assert True
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login field is not"
-        #assert True
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register field is not"
-        #assert True
